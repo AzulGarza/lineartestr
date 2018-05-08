8dda870aebbc4b5baf1aceb05eb65f27ccfa95f6
Federico Garza <fede.garza.ramirez@gmail.com>
2018-05-08 00:13:32 -0500

    Adding model matrix

--- R/asyvar.R
+++ R/asyvar.R
@@ -12,15 +12,16 @@ asy_var <- function(model, robust = F){
     stop("Model must be a lm model")
   }
   resids <- model$residuals
-  data <- data.matrix(independent_data(model))
+  data_m <- model.matrix(formula(model$terms), model$model)
+  #data <- data.matrix(independent_data(model))
   n_obs <- nrow(data)
-  if(as.logical(attr(model$terms, "intercept"))){
+  #if(as.logical(attr(model$terms, "intercept"))){
     # Adding ones if is necesary
-    intercept <- rep(1, n_obs)
-    setNames(intercept, "intercept")
-    data_m <- cbind(intercept, data)
-  } else {
-    data_m <- data
+  #  intercept <- rep(1, n_obs)
+  #  setNames(intercept, "intercept")
+  #  data_m <- cbind(intercept, data)
+  #} else {
+  #  data_m <- data
   }
   var <- solve(t(data_m)%*%data_m)
   if(!robust){
